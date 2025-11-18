69ee264c0a6c56c86191813fe066d68932b74c12
samiudeen96 <samiudeen96amm@gmail.com>
2025-11-18 12:13:27 +0400

    update

--- src/components/pages/home/Hero.jsx
+++ src/components/pages/home/Hero.jsx
@@ -20,71 +20,76 @@ const Hero = () => {
   };
 
   useGSAP(() => {
-  gsap.timeline()
-    .to(".text1", { opacity: 1, delay: 1, ease: "power1.inOut" })
-    .to(".text1", { y: 20, duration: 1.3, ease: "power1.inOut" });
+    gsap
+      .timeline()
+      .to(".text1", { opacity: 1, delay: 1, ease: "power1.inOut" })
+      .to(".text1", { y: 20, duration: 1.3, ease: "power1.inOut" });
 
-  // Product initial scroll animation
-  gsap.to(productWrapperRef.current, {
-    y: 350,
-    ease: "power1.inOut",
-    scrollTrigger: {
-      trigger: productWrapperRef.current,
-      start: "top center",
-      end: "top top",
-      scrub: true,
-    },
-  });
+    // Product initial scroll animation
+    gsap.to(productWrapperRef.current, {
+      y: 300,
+      ease: "power1.inOut",
+      scrollTrigger: {
+        trigger: productWrapperRef.current,
+        start: "top center-=20%",
+        end: "top top",
+        scrub: true,
+      },
+    });
 
-  // Hidden text fade in
-  gsap.to(".hiddenText", {
-    opacity: 1,
-    scrollTrigger: {
-      trigger: ".hiddenText",
-      start: "top center",
-      end: "center center",
-      scrub: true,
-    },
-  });
+    // Hidden text fade in
+    gsap.to(".hiddenText", {
+      opacity: 1,
+      scrollTrigger: {
+        trigger: ".hiddenText",
+        start: "top center",
+        end: "bottom center",
+        scrub: true,
+        pin: true,
+      },
+    });
 
-  // ---------------------------
-  // 🚫 REMOVED containerRef pin
-  // ---------------------------
+    // ---------------------------
+    // ✔️ Single pin timeline
+    // ---------------------------
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: ".hiddenText",
+        start: "top top",
+        end: "bottom+=500 top+=40%",
+        scrub: true,
+        pin: true,
+      },
+    });
 
-  // ---------------------------
-  // ✔️ Single pin timeline
-  // ---------------------------
-  const tl = gsap.timeline({
-    scrollTrigger: {
-      trigger: ".hiddenText",
-      start: "top top",
-      end: "bottom+=500 top",
-      scrub: true,
-      pin: true,
-    },
-  });
+    tl.to(productWrapperRef.current, {
+      y: 300,
+      duration: 1,
+      ease: "power1.out",
+    });
+    tl.to(productWrapperRef.current, {
+      x: 280,
+      y: -50,
+      duration: 2,
+      ease: "power1.in",
+    });
 
-  tl.to(productWrapperRef.current, { y: 350, duration: 1, ease: "power1.inOut" });
-  tl.to(productWrapperRef.current, { x: 250, y: -50, duration: 1, ease: "power1.inOut" });
-
-  // Pin scroll sections
-  scrollRefs.current.forEach((section) => {
-    ScrollTrigger.create({
-      trigger: section,
-      start: "top top",
-      end: "bottom top",
-      pin: true,
-      pinSpacing: true,
+    // Pin scroll sections
+    scrollRefs.current.forEach((section) => {
+      ScrollTrigger.create({
+        trigger: section,
+        start: "top top",
+        end: "bottom top",
+        pin: true,
+        pinSpacing: true,
+      });
     });
   });
-});
-
 
   return (
     <section className="relative">
-
       {/* ---------------- SECTION 1 ---------------- */}
-      <div className="relative h-[calc(100vh-380px)] w-full">
+      <div className="relative h-[calc(100vh-64px)] w-full">
         <h1 className="text1 opacity-0 translate-y-[38vh]">
           Your healthiest <br /> skin revealed.
         </h1>
@@ -93,12 +98,10 @@ const Hero = () => {
       {/* ---------------- SECTION 2 ---------------- */}
       <div
         ref={containerRef}
-        className="flex items-start justify-center h-[100vh]"
+        className="flex items-start justify-center h-[100vh] bg-amber-100"
       >
         <h1 className="hiddenText opacity-0">HiddenText</h1>
       </div>
-
-
 
       {/* ---------------- PRODUCT IMAGE ---------------- */}
       <div
@@ -116,16 +119,19 @@ const Hero = () => {
 
       {/* ---------------- SCROLL SECTIONS ---------------- */}
       {scrollContent.map((item, index) => (
-        <div key={index} className="h-screen  flex items-center" ref={addToRefs}>
-          <div className="w-8/12 mx-auto">
-            <div className="w-4/12">
+        <div
+          key={index}
+          className="h-screen  flex items-center"
+          ref={addToRefs}
+        >
+          <div className="w-11/12 mx-auto">
+            <div className="w-5/12">
               <h4>{item.title}</h4>
               <p>{item.content}</p>
             </div>
           </div>
         </div>
       ))}
-
     </section>
   );
 };
