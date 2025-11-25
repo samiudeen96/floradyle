d81129d2f8ae960037dc76d70775313540c93d2c
samiudeen96 <samiudeen96amm@gmail.com>
2025-11-25 12:01:55 +0400

    update

--- src/components/pages/home/Hero.jsx
+++ src/components/pages/home/Hero.jsx
@@ -70,7 +70,7 @@ const Hero = () => {
 
       ScrollTrigger.create({
         trigger: ".product-pin-section",
-        start: "center center+=32px", 
+        start: "center-=32px center", 
         endTrigger: ".sectionEnd",
         end: "top bottom", // pin until all sections scroll past
         pin: true,
